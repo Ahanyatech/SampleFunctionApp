0fde35a1313237e1a9d6e97e421fdb74c662d31d
Saurabh Agarwal <saurabh.agarwal@ahanyatech.com>
2022-11-02 15:51:53 -0500

    Added Azure App Configuration Code

--- Function1.cs
+++ Function1.cs
@@ -7,29 +7,55 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System.Linq;
+using Microsoft.FeatureManagement;
+using Microsoft.Extensions.Configuration.AzureAppConfiguration;
+using Azure.Data.AppConfiguration;
+using Microsoft.Extensions.Configuration;
 
 namespace SampleFunctionApp
 {
-    public static class AccessKeyVault
+    public class AccessKeyVault
     {
+        private readonly IConfiguration _configuration;
+        private readonly IFeatureManagerSnapshot _featureManagerSnapshot;
+        private readonly IConfigurationRefresher _configurationRefresher;
+
+        public AccessKeyVault(IConfiguration configuration, IFeatureManagerSnapshot featureManagerSnapshot, IConfigurationRefresherProvider refresherProvider)
+        {
+            _configuration = configuration;
+            _featureManagerSnapshot = featureManagerSnapshot;
+            _configurationRefresher = refresherProvider.Refreshers.First();
+        }
+
         [FunctionName("AccessKeyVault")]
-        public static async Task<IActionResult> Run(
+        public async Task<IActionResult> Run(
             [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
             ILogger log)
         {
             log.LogInformation("Function: AccessKeyVault Execution Started");
 
-            string name = req.Query["key"];
+            await _configurationRefresher.TryRefreshAsync();
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
+            string message = await _featureManagerSnapshot.IsEnabledAsync("Beta")
+                    ? "The Feature Flag 'Beta' is turned ON"
+                    : "The Feature Flag 'Beta' is turned OFF";
+            string key = "Environment";
+            string config = _configuration[key];
+            message = message + config;
+            return (ActionResult)new OkObjectResult(message);
 
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "No key has been passed"
-                : $"Value for the key: {name} has been successfully retrieved and it's value is.";
+            //string name = req.Query["key"];
 
-            return new OkObjectResult(responseMessage);
+            //string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            //dynamic data = JsonConvert.DeserializeObject(requestBody);
+            //name = name ?? data?.name;
+
+            //string responseMessage = string.IsNullOrEmpty(name)
+            //    ? "No key has been passed"
+            //    : $"Value for the key: {name} has been successfully retrieved and it's value is.";
+
+            //return new OkObjectResult(responseMessage);
         }
     }
 }
---
+++ Startup.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+using Microsoft.Azure.Functions.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.FeatureManagement;
+
+[assembly: FunctionsStartup(typeof(SampleFunctionApp.Startup))]
+namespace SampleFunctionApp
+{
+    class Startup : FunctionsStartup
+    {
+        public override void ConfigureAppConfiguration(IFunctionsConfigurationBuilder builder)
+        {
+            builder.ConfigurationBuilder.AddAzureAppConfiguration(options =>
+            {
+                options.Connect("Endpoint=https://adventhealth-app-config.azconfig.io;Id=X5EZ-l0-s0:wPQUTygmS1TaZAQ0332b;Secret=xdXSyd9ijW76rsNVs7Ck9eQPIGUWFKS54Gf21iulW2A=")
+                .UseFeatureFlags();
+            });
+        }
+
+        public override void Configure(IFunctionsHostBuilder builder)
+        {
+            // Make Azure App Configuration services and feature manager available through dependency injection
+            builder.Services.AddAzureAppConfiguration();
+            builder.Services.AddFeatureManagement();
+        }
+    }
+}
